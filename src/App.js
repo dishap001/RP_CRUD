1f2809d5083cea22c85eed2d9b93d322028429bf
dishap001 <pdisha@trellissoft.ai>
2023-10-25 13:35:20 +0530

    Created components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import Create from './Components/Create';
+import Read from './Components/Read';
+import Update from './Components/Update';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      
-        <p>hello world</p>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/update" element={<Update />} />
+          <Route path="/read" element={<Read />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='d-flex flex-column justify-content align-items-center bg-light vh-100'>
+        <h1>List of Users</h1>
+        <div className='w-75 rounded bg-white border shadow p-4'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Home
